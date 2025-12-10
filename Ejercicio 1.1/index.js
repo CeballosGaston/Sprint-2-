16578fe851afe79997a8197ab5d7f74b73fd39e2
Gastón Ceballos <ceballoscallerosgaston@gmail.com>
2025-12-10 14:45:57 +0100

    Ejercicio 1 Agregado

---
+++ Ejercicio 1.1/index.js
@@ -0,0 +1,24 @@
+"use strict";
+
+
+//^^^^^^^^^^^^^EJERCICIO 1.1^^^^^^^^^^^^^^^
+//================NIVEL 1 =================
+
+// -------------Ejercicio 1--------------
+
+function add(a, b) {
+  return a + b;
+}
+
+const addArrow = (a, b) => a + b;
+
+console.log(add(4, 5));
+const newResult = addArrow(4, 5);
+
+console.log(newResult);
+
+
+
+
+
+
