6873da350b0da722849e63f2a1b7fcd4a7745c7f
Gastón Ceballos <ceballoscallerosgaston@gmail.com>
2025-12-10 15:30:48 +0100

    Ejercicio 3 agregado

--- Ejercicio 1.2/index.js
+++ Ejercicio 1.2/index.js
@@ -23,3 +23,26 @@ function numeroMayor(num1, num2) {
     ? console.log("El segundo número es más grande")
     : console.log("Son iguales");
 }
+
+
+//================NIVEL 2 =================
+
+// -------------Ejercicio 3-----------------
+
+function numeroPositivoONegativo(num) {
+  num > 0
+    ? console.log("El número es positivo")
+    : num < 0
+    ? console.log("El número es negativo")
+    : console.log("El número es cero");
+}
+
+function trobarMaxim(a, b, c) {
+  a > b && a > c
+    ? console.log(`El número ${a} es el mayor`)
+    : b > a && b > c
+    ? console.log(`El número ${b} es el mayor`)
+    : c > a && c > b
+    ? console.log(`El número ${c} es el mayor`)
+    : console.log(`Los número son iguales`);
+}
