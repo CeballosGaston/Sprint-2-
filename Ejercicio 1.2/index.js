820790a16c45ad2c66ee42e2d8cf3ecfab335494
Gastón Ceballos <ceballoscallerosgaston@gmail.com>
2025-12-10 15:29:49 +0100

    Ejercicio 1 agregado

---
+++ Ejercicio 1.2/index.js
@@ -0,0 +1,15 @@
+"use strict";
+
+//^^^^^^^^^^^^^EJERCICIO 1.2^^^^^^^^^^^^^^^
+//================NIVEL 1 =================
+
+// -------------Ejercicio 1-----------------
+
+function potConduir(age) {
+  return age > 18
+    ? console.log("Pots conduir")
+    : console.log("No pots conduir");
+}
+
+potConduir(44);
+
