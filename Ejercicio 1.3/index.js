1603fcbe71dde261171f951e54edcb9a0e2c511a
Gastón Ceballos <ceballoscallerosgaston@gmail.com>
2025-12-10 15:45:51 +0100

    Ejercicio 1 Agregado

---
+++ Ejercicio 1.3/index.js
@@ -0,0 +1,12 @@
+"use strict";
+
+//^^^^^^^^^^^^^EJERCICIO 1.3^^^^^^^^^^^^^^^
+
+//================NIVEL 1 =================
+
+// -------------Ejercicio 1-----------------
+
+function processar(num, callback) {
+  console.log(callback(num));
+}
+
