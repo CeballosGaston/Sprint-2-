cd0c1276aa840ae2e03d61e303ca23ef1b41fd55
Gastón Ceballos <ceballoscallerosgaston@gmail.com>
2025-12-10 15:46:13 +0100

    Ejercicio 2 agregado

--- Ejercicio 1.3/index.js
+++ Ejercicio 1.3/index.js
@@ -10,3 +10,12 @@ function processar(num, callback) {
   console.log(callback(num));
 }
 
+// -------------Ejercicio 2-----------------
+
+function calculadora(a, b, callback) {
+  console.log(callback(a, b));
+}
+
+const sumar = (a, b) => a + b;
+
+calculadora(23, 23, sumar);
