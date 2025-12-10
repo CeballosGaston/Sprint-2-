3dfe28405f48055a57096c73f3eeb6c8ffdcdffb
Gastón Ceballos <ceballoscallerosgaston@gmail.com>
2025-12-10 15:30:21 +0100

    Ejercicio 2 agregado

--- Ejercicio 1.2/index.js
+++ Ejercicio 1.2/index.js
@@ -13,3 +13,13 @@ function potConduir(age) {
 
 potConduir(44);
 
+
+// -------------Ejercicio 2-----------------
+
+function numeroMayor(num1, num2) {
+  num1 > num2
+    ? console.log("El primer número es más grande")
+    : num2 > num1
+    ? console.log("El segundo número es más grande")
+    : console.log("Son iguales");
+}
