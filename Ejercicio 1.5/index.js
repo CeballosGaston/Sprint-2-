7a26caf379a890f9f1587902d0799efac8078ada
Gastón Ceballos <ceballoscallerosgaston@gmail.com>
2025-12-11 10:05:35 +0100

    Ejercicio 1 agregado

---
+++ Ejercicio 1.5/index.js
@@ -0,0 +1,13 @@
+"use strict";
+
+//^^^^^^^^^^^^^EJERCICIO 1.5^^^^^^^^^^^^^^^
+
+//================NIVEL 1 =================
+
+// -------------Ejercicio 1-----------------
+
+const arrayNumeros = [1, 2, 3, 4];
+
+const cuadrados = arrayNumeros.map((x) => x * x);
+console.log(cuadrados);
+
