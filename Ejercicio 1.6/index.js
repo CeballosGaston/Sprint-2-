f01cb27710ddb1752c262b022d486c3c56599944
Gastón Ceballos <ceballoscallerosgaston@gmail.com>
2025-12-11 10:10:54 +0100

    Ejercicio 1 actualizado

---
+++ Ejercicio 1.6/index.js
@@ -0,0 +1,14 @@
+"use strict";
+
+//^^^^^^^^^^^^^EJERCICIO 1.6^^^^^^^^^^^^^^^
+
+//================NIVEL 1 =================
+
+// -------------Ejercicio 1-----------------
+
+const nombres = ["Anna", "Bernat", "Clara"];
+
+nombres.forEach((n) => {
+  console.log(n);
+});
+
