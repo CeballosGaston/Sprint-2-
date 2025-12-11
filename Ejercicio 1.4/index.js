11c01f46a7477680cd8e98103f4a8c44eedce16f
Gastón Ceballos <ceballoscallerosgaston@gmail.com>
2025-12-11 10:01:31 +0100

    Ejercicio 2 agregado

--- Ejercicio 1.4/index.js
+++ Ejercicio 1.4/index.js
@@ -12,3 +12,15 @@ const array2 = [6, 7, 8, 9, 10];
 const array3 = [...array1, ...array2];
 
 console.log(array3);
+
+// -------------Ejercicio 2-----------------
+
+function suma(...numeros) {
+  return numeros.reduce((total, numero) => total + numero, 0);
+}
+
+//PROBÁNDOLO
+
+const array4 = [3, 4, 5, 6, 76, 45];
+console.log(suma(1, 2, 3, 4, 5, 6));
+console.log(suma(...array4));
