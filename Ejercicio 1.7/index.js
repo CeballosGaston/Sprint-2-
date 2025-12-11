ed9d556a1f57b97a87eed476baada0941aa5fe10
Gastón Ceballos <ceballoscallerosgaston@gmail.com>
2025-12-11 10:14:25 +0100

    Ejercicio 3 agregado

--- Ejercicio 1.7/index.js
+++ Ejercicio 1.7/index.js
@@ -16,3 +16,20 @@ const promesa = new Promise((resolve) => {
 
 promesa.then((mensaje) => console.log(mensaje));
 
+// -------------Ejercicio 3-----------------
+
+function verificar(input) {
+  return new Promise((resolve, reject) => {
+    if (input !== "Hola") {
+      return reject("El input no es el correcto");
+    }
+
+    setTimeout(() => {
+      resolve("Hola");
+    }, 2000);
+  });
+}
+
+verificar("Hola")
+  .then((msg) => console.log(msg))
+  .catch((err) => console.log(err));
