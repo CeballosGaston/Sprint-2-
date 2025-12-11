4742f4947fe03b391ea7ddb71051630a58c09ae9
Gastón Ceballos <ceballoscallerosgaston@gmail.com>
2025-12-11 10:01:04 +0100

    Ejercicio 1 agregado

---
+++ Ejercicio 1.4/index.js
@@ -0,0 +1,14 @@
+"use stric";
+
+//^^^^^^^^^^^^^EJERCICIO 1.4^^^^^^^^^^^^^^^
+
+//================NIVEL 1 =================
+
+// -------------Ejercicio 1-----------------
+
+const array1 = [1, 2, 3, 4, 5];
+const array2 = [6, 7, 8, 9, 10];
+
+const array3 = [...array1, ...array2];
+
+console.log(array3);
