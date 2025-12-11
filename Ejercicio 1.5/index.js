cde3a94d7e1b4bd6f29891d1fca2da6a6236a4cf
Gastón Ceballos <ceballoscallerosgaston@gmail.com>
2025-12-11 10:05:53 +0100

    Ejercicio 2 agregado

--- Ejercicio 1.5/index.js
+++ Ejercicio 1.5/index.js
@@ -11,3 +11,7 @@ const arrayNumeros = [1, 2, 3, 4];
 const cuadrados = arrayNumeros.map((x) => x * x);
 console.log(cuadrados);
 
+// -------------Ejercicio 2-----------------
+
+const pares = arrayNumeros.filter((x) => x % 2 == 0);
+console.log(pares);
